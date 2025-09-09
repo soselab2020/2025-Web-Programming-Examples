3b127da0ebf476746cb2660d096f6af2c9055b20
Albert Ma <shangpinma@gmail.com>
2025-09-09 10:30:17 +0800

    fix: remove advanced code examples

--- node-examples/demo_http_url.js
+++
@@ -1,8 +0,0 @@
-//demo_http_url
-const http = require('http');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(req.url);
-    res.end();
-}).listen(8080);
--- node-examples/demo_uppercase.js
+++
@@ -1,8 +0,0 @@
-//demo_uppercase
-const http = require('http');
-const uc = require('upper-case');
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(uc.upperCase("Hello World!"));
-    res.end();
-}).listen(8080);
